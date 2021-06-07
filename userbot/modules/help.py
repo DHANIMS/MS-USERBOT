627e87ea710d16a50a53f4727deafc062f946af2
DHANIMS <85518061+DHANIMS@users.noreply.github.com>
2021-06-08 02:48:25 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -32,11 +32,11 @@ async def help(event):
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\t 💫  "
-        await event.edit("**⭐𝗥𝗔𝗠-𝗨𝗕𝗢𝗧⭐**\n\n"
-                         f"**💫 𝙿𝙴𝙼𝙸𝙻𝙸𝙺 𝙱𝙾𝚃 : {DEFAULTUSER}**\n**💫  𝙼𝙾𝙳𝚄𝙻𝙴𝚂 : {len(modules)}**\n\n"
-                         "**💫 𝚂𝙴𝙼𝚄𝙰 𝙼𝙴𝙽𝚄 :**\n\n ═════════════╣❃ ♕ ❃╠═════════════\n\n"
-                         f"💫 {string}\n\n ═════════════╣❃ ♕ ❃╠═════════════\n\nNGETIK YANG BENER YA NGENTOOOOT!!\n\n")
+            string += "`\t 🔥  "
+        await event.edit("**☠️MS-USERBOT**\n\n"
+                         f"**💀 𝙿𝙴𝙼𝙸𝙻𝙸𝙺 𝙱𝙾𝚃 : {DEFAULTUSER}**\n**☠️  𝙼𝙾𝙳𝚄𝙻𝙴𝚂 : {len(modules)}**\n\n"
+                         "**☠️𝚂𝙴𝙼𝚄𝙰 𝙼𝙴𝙽𝚄 :**\n\n ═════════════╣❃ ♕ ❃╠═════════════\n\n"
+                         f"💀 {string}\n\n ═════════════╣❃ ♕ ❃╠═════════════\n\nNGETIK YANG BENER YA NGENTOOOOT!!\n\n")
         await event.reply(f"\n**Contoh** : Ketik <`.help ping`> Untuk Informasi Pengunaan.\nJangan Lupa Berdoa Sebelum Mencoba wahahaha..")
         await asyncio.sleep(50)
         await event.delete()
